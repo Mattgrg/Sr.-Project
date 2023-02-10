7644470e5fd68e3c764aeffbe4206dc1cda95c42
jwCheng28 <chengjw68@gmail.com>
2023-02-10 13:11:14 -0800

    Change post request to connect to command bluetooth device

--- user_app/app/api/user_app_backend.py
+++ user_app/app/api/user_app_backend.py
@@ -1,41 +1,46 @@
 from flask import Flask
 from flask import request
-import requests
+import bluetooth
+# import requests
 
 from . import backend_api
-from app.utils.configs import Smart_Hub_API
+# from app.utils.configs import Smart_Hub_API
 
-valid_devices = set()
 running_devices = set()
 
 @backend_api.route('/')
 def home():
     return "Home Page"
 
+def scan_nearby_bluetooth():
+    return {name : addr for addr, name in bluetooth.discover_devices(lookup_names=True)}
+
+def send_bluetooth_signal(dev_addr, port, msg):
+    sock = bluetooth.BluetoothSocket()
+    print("connecting to", dev_addr)
+    sock.connect((dev_addr, port))
+    sock.send("CONNECT")
+    sock.close()
+
 @backend_api.route("/device/command", methods = ["POST"])
 def device_command():
     comm = request.get_json()
     device, command = list(comm.items())[0]
+    avail_bluetooth_devs = scan_nearby_bluetooth()
+    print("Available Bluetooh:", avail_bluetooth_devs)
 
-    if device not in valid_devices:
-        ret = requests.post(Smart_Hub_API.PING, data={device:1})
-        if ret == "FAIL":
-            return "Device Not Valid"
-        valid_devices.add(device)
+    if device not in avail_bluetooth_devs:
+        return "Device Not Found"
 
     if device in running_devices:
         if command == "ON":
             return "Device is already On"
-        ret = requests.post(Smart_Hub_API.COMMAND, data=comm)
-        if ret == "FAIL":
-            return "Device Turn Off Failed"
+        send_bluetooth_signal(avail_bluetooth_devs[device], 1, command)
         running_devices.remove(device)
     else:
         if command == "OFF":
             return "Device is already Off"
-        ret = requests.post(Smart_Hub_API.COMMAND, data=comm)
-        if ret == "FAIL":
-            return "Device Turn On Failed"
+        send_bluetooth_signal(avail_bluetooth_devs[device], 1, command)
         running_devices.add(device)
     return f"Device Turn {command.title()} Success"
 
@@ -43,5 +48,4 @@ def device_command():
 def device_info():
     return {
             "running_dev" : list(running_devices),
-            "valid_dev" : list(valid_devices)
             }
