71f5393cfbc5549c5b2df2bad07e826bae78f0b3
Matthew Gragg <Matthew Gragg>
2023-02-17 11:13:38 -0800

    Setup script version 1: initial commit.

--- user_app_fontend/front_end_setup.sh
+++ user_app_fontend/front_end_setup.sh
@@ -2,4 +2,14 @@ curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 
 sudo apt-get install -y nodejs
 
+sudo apt-get install -y npm
+
 node -v
+
+echo node -V >> version.txt
+
+cd ~/hub_s/front_end/
+
+node -nb webserver_fe.js
+
+cat version.txt
